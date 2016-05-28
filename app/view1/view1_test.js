aca6ed3d74df5c3619b937262eb1a297b79e6d12
ramit21 <21.ramit@gmail.com>
2016-05-28 18:25:26 +0530

    unit test

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -4,25 +4,32 @@ describe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
 
-   var scope;
-    beforeEach(inject(function($rootScope, $controller) {
+   var scope, controller, httpBackend;
+
+    beforeEach(inject(function($rootScope, $controller, $httpBackend) {
         scope = $rootScope.$new();
-        $controller("View1Ctrl", {
+        controller = $controller;
+        /*$controller("View1Ctrl", {
             $scope: scope
-        });
+        });*/
+        httpBackend = $httpBackend;
     }));
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
+    it('controller defined', inject(function($controller) {
+      //Controller injected directly
       var view1Ctrl = $controller('View1Ctrl',{$scope: scope});
       expect(view1Ctrl).toBeDefined();
     }));
 
-    it("value of var", function() {
+    it("value of Scope variable", function() {
+        var view1Ctrl = controller('View1Ctrl',{$scope: scope});
         expect(scope.var).toBe(100);
      });
+
+    xit("http success", function() {
+    });
 
   });
 });
